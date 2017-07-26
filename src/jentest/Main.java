a542823ec1d5d07a98e880c7cdd0aa6be4043d6c
gokulmInfy <gokul_madhavan@infosys.com>
2017-07-26 18:36:05 +0530

    Update.

--- src/jentest/Main.java
+++ src/jentest/Main.java
@@ -10,7 +10,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONException;
+import org.json.simple.parser.ParseException;
 
 public class Main {
 
@@ -49,7 +49,7 @@ public class Main {
 					JSONObject json = (JSONObject) new JSONParser().parse(output);
 					String x=(String) json.get("report_id");
 					System.out.println("---report_id "+x);
-				 } catch (JSONException e) {
+				 } catch (ParseException e) {
     				   	System.out.println("--some exception handler code.-");
 					e.printStackTrace();
  			  	 }  
