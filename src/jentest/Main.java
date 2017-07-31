d3655369df2ba8077de4280c3998611364ce867e
gokulmInfy <gokul_madhavan@infosys.com>
2017-07-31 10:26:46 +0530

    updated with imports and onsite changes

--- src/jentest/Main.java
+++ src/jentest/Main.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.simple.JSONObject;
@@ -21,101 +23,89 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World \n");
-		String x = null;
+		String report_id = null;
 		
-		 try {
-
-				DefaultHttpClient httpClient = new DefaultHttpClient();
-				HttpPost postRequest = new HttpPost(
-					"http://localhost:8000/execution_task/");
-
-				StringEntity input = new StringEntity("{\"auth_token\": \"ac2e1ab0-0805-45df-af1f-a82f070ba9b7\", \"job_id\": 1}");
-				input.setContentType("application/json");
-				postRequest.setEntity(input);
-
-				HttpResponse response = httpClient.execute(postRequest);
-
-				if (response.getStatusLine().getStatusCode() != 200) {
-					throw new RuntimeException("Failed : HTTP error code : "
-						+ response.getStatusLine().getStatusCode());
-				}
-
-				BufferedReader br = new BufferedReader(
-		                        new InputStreamReader((response.getEntity().getContent())));
-
-				String output;
-				StringBuilder jsonBuilder = new StringBuilder();
-				System.out.println("Output from Server .... \n");
-				while ((output = br.readLine()) != null) {
-					jsonBuilder.append(output);
-					
-				}
-				System.out.println(jsonBuilder.toString());
-				httpClient.getConnectionManager().shutdown();
-				 try {
-					JSONObject json = (JSONObject) new JSONParser().parse(jsonBuilder.toString());
-					x=(String) json.get("report_id");
-					System.out.println("---report_id "+x);
-				 } catch (ParseException e) {
-    				   	System.out.println("--some exception handler code.-");
-					e.printStackTrace();
- 			  	 }
-
-			  } catch (MalformedURLException e) {
-
-				e.printStackTrace();
-
-			  } catch (IOException e) {
-
-				e.printStackTrace();
-
-			  }
-		 boolean report_ready = false;
-		 while(!report_ready){
-			 String status = makeGetcall(x);
-			 if(status != null && status.contains("true")){
-				 report_ready = true; 
-			 }else{
-				 TimeUnit.SECONDS.sleep(5);
-			 }
-		 }
-			Properties prop = new Properties();
-			OutputStream output1 = null;
-			try {
-
-				output1 = new FileOutputStream("config.properties");
-
-				// set the properties value
-				prop.setProperty("awesomeness", "!");
-				prop.setProperty("report_id", x);
-				// save properties to project root folder
-				prop.store(output1, null);
-
-			} catch (IOException io) {
-				io.printStackTrace();
-			} finally {
-				if (output1 != null) {
-					try {
-						output1.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-
-			}
-
-			}
-
-	private static String makeGetcall(String reportid) {
-		String status = null;
 		try {
-			
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost(
+				"http://localhost:8000/execution_task/");
+			StringEntity input = new StringEntity("{\"auth_token\": \"ac2e1ab0-0805-45df-af1f-a82f070ba9b7\", \"job_id\": 1}");
+			input.setContentType("application/json");
+			postRequest.setEntity(input);
+			HttpResponse response = httpClient.execute(postRequest);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = new BufferedReader(
+				new InputStreamReader((response.getEntity().getContent())));
+
+			String output;
+			StringBuilder jsonBuilder = new StringBuilder();
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				jsonBuilder.append(output);
+			}
+			System.out.println(jsonBuilder.toString());
+			httpClient.getConnectionManager().shutdown();
+			try {
+				JSONObject json = (JSONObject) new JSONParser().parse(jsonBuilder.toString());
+				report_id=(String) json.get("report_id");
+				System.out.println("---report_id "+report_id);
+			} catch (ParseException e) {
+				System.out.println("--some exception handler code.-");
+				e.printStackTrace();
+ 			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if ( report_id != null ) {
+			boolean report_ready = false;
+		 	while(!report_ready){
+				boolean report_status = getReportStatus(report_id);
+			 	if(report_status){
+					report_ready = true;
+			 	}else{
+					try {
+				 		TimeUnit.SECONDS.sleep(10);
+				 	} catch(InterruptedException e){
+				 		e.printStackTrace();
+				 	}
+			 	}
+			}
+		}
+		Properties prop = new Properties();
+		OutputStream output1 = null;
+		try {
+			output1 = new FileOutputStream("config.properties");
+			// set the properties value
+			prop.setProperty("report_id", report_id);
+			// save properties to project root folder
+			prop.store(output1, null);
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (output1 != null) {
+				try {
+					output1.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private static boolean getReportStatus(String reportid) {
+		boolean report_status = false;
+		try {
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpGet getRequest = new HttpGet(
-				"http://localhost:8000/report_test/reportid");
+				"http://localhost:8000/report_test/" + reportid );
 			getRequest.addHeader("accept", "application/json");
-
 			HttpResponse response = httpClient.execute(getRequest);
 
 			if (response.getStatusLine().getStatusCode() != 200) {
@@ -125,7 +115,6 @@ public class Main {
 
 			BufferedReader br = new BufferedReader(
 	                         new InputStreamReader((response.getEntity().getContent())));
-
 			String output;
 			StringBuilder jsonBuilder = new StringBuilder();
 			System.out.println("Output from Server .... \n");
@@ -135,26 +124,19 @@ public class Main {
 			}
 			System.out.println("get response: "+ jsonBuilder.toString());
 			httpClient.getConnectionManager().shutdown();
-			 try {
-					JSONObject json = (JSONObject) new JSONParser().parse(jsonBuilder.toString());
-					status=(String) json.get("status");
-					System.out.println("---status "+status);
-				 } catch (ParseException e) {
- 				   	System.out.println("--GET some exception handler code.-");
-					e.printStackTrace();
-			  	 }
-
-		  } catch (ClientProtocolException e) {
-
+			try {
+				JSONObject json = (JSONObject) new JSONParser().parse(jsonBuilder.toString());
+				report_status = (boolean)json.get("status");
+				System.out.println("---report_status "+report_status);
+			} catch (ParseException e) {
+				System.out.println("--GET some exception handler code.-");
+				e.printStackTrace();
+			}
+		} catch (ClientProtocolException e) {
 			e.printStackTrace();
-
-		  } catch (IOException e) {
-
+		} catch (IOException e) {
 			e.printStackTrace();
-		  }
-		
-		return status;
-		
+		}
+		return report_status;
 	}
-	
 }
