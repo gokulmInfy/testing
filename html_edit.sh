e34e73fc2a76cb323703f99ad3bcc9bfbe41bc12
gokulmInfy <gokul_madhavan@infosys.com>
2017-08-01 10:43:44 +0530

    updated again

--- html_edit.sh
+++ html_edit.sh
@@ -1,5 +1,5 @@
-sed -i '/<div class="navbar-header">/,/<\/div>/d' ./reports/index.html
-sed -i '/<div class="navbar-collapse collapse">/,/<\/div>/d' ./reports/index.html
-sed -i '/<footer>/,/<\/footer>/d' ./reports/index.html
-sed -i 's/href=\"[^"]*\"//g' ./reports/index.html
+sed -i '/<div class="navbar-header">/,/<\/div>/d' reports/index.html
+sed -i '/<div class="navbar-collapse collapse">/,/<\/div>/d' reports/index.html
+sed -i '/<footer>/,/<\/footer>/d' reports/index.html
+sed -i 's/href=\"[^"]*\"//g' reports/index.html
 
