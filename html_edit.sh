862ae83859984b5de4f23c793fc1e836e766563b
gokulmInfy <gokul_madhavan@infosys.com>
2017-08-01 16:21:58 +0530

    updated script to work without -i

--- html_edit.sh
+++ html_edit.sh
@@ -1,5 +1,8 @@
-sed -i '/<div class="navbar-header">/,/<\/div>/d' reports/index.html
-sed -i '/<div class="navbar-collapse collapse">/,/<\/div>/d' reports/index.html
-sed -i '/<footer>/,/<\/footer>/d' reports/index.html
-sed -i 's/href=\"[^"]*\"//g' reports/index.html
-
+sed -e '/<div class="navbar-header">/,/<\/div>/d' reports/index.html > reports/index1.tmp
+mv reports/index1.tmp reports/index.html
+sed -e '/<div class="navbar-collapse collapse">/,/<\/div>/d' reports/index.html > reports/index1.tmp
+mv reports/index1.tmp reports/index.html
+sed -e '/<footer>/,/<\/footer>/d' reports/index.html > reports/index1.tmp
+mv reports/index1.tmp reports/index.html
+sed -e 's/href=\"[^"]*\"//g' reports/index.html > reports/index1.tmp
+mv reports/index1.tmp reports/index.html
