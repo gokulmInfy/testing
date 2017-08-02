dba85741172d93e7d3d83a29bce55421a43d9705
gokulmInfy <gokul_madhavan@infosys.com>
2017-08-02 18:39:05 +0530

    updated with html changes

--- html_edit.sh
+++ html_edit.sh
@@ -1,9 +1,11 @@
-sed -e '/<div class="navbar-header">/,/<\/div>/d' reports/index.html > reports/index1.tmp
-mv reports/index1.tmp reports/index.html
-sed -e '/<div class="navbar-collapse collapse">/,/<\/div>/d' reports/index.html > reports/index1.tmp
-mv reports/index1.tmp reports/index.html
-sed -e '/<footer>/,/<\/footer>/d' reports/index.html > reports/index1.tmp
-mv reports/index1.tmp reports/index.html
-sed -e 's/\/static\//\.\/static\//g' reports/index.html > reports/index1.tmp
+sed -e '/<div class="navbar-header">/,/<\/div>/d' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e '/<div class="navbar-collapse collapse">/,/<\/div>/d' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e '/<footer>/,/<\/footer>/d' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
 #sed -e 's/href=\"[^"]*\"//g' reports/index.html > reports/index1.tmp
-mv reports/index1.tmp reports/index.html
+sed -e 's/\"\_blank\" href=\"[^"]*\"//g' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e 's/\/static\//\.\/static\//g' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
