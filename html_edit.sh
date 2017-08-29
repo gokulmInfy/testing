9adb94eb4a54b2b6b0ac6a739c150bcd63a09b96
gokulmInfy <gokul_madhavan@infosys.com>
2017-08-29 16:43:40 +0530

    updated sh to remove dynamic pagination
    updated sh to remove dynamic pagination

--- html_edit.sh
+++ html_edit.sh
@@ -2,10 +2,17 @@ sed -e '/<div class="navbar-header">/,/<\/div>/d' ./reports/index.html > ./repor
 mv ./reports/index1.tmp ./reports/index.html
 sed -e '/<div class="navbar-collapse collapse">/,/<\/div>/d' ./reports/index.html > ./reports/index1.tmp
 mv ./reports/index1.tmp ./reports/index.html
+sed -e '/<div class="container-fluid">/,/<\/div>/d' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e '/<div class="navbar navbar-default"/,/<\/div>/d' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
 sed -e '/<footer>/,/<\/footer>/d' ./reports/index.html > ./reports/index1.tmp
 mv ./reports/index1.tmp ./reports/index.html
-#sed -e 's/href=\"[^"]*\"//g' reports/index.html > reports/index1.tmp
 sed -e 's/\"\_blank\" href=\"[^"]*\"//g' ./reports/index.html > ./reports/index1.tmp
 mv ./reports/index1.tmp ./reports/index.html
-sed -e 's/\/static\//\.\/static\//g' ./reports/index.html > ./reports/index1.tmp
+sed -e 's/row/row1/g' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e 's/dataTable/dataTable111/g' ./reports/index.html > ./reports/index1.tmp
+mv ./reports/index1.tmp ./reports/index.html
+sed -e 's/\/static\//\.ws\/reports\/\/static\//g' ./reports/index.html > ./reports/index1.tmp
 mv ./reports/index1.tmp ./reports/index.html
